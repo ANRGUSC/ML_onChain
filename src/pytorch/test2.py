81b92c1b55bc4c8c56e681e77a647939bc1dbaf2
Steve Vott <svott@usc.edu>
2023-06-08 15:43:41 -0700

    more parsing

--- src/pytorch/test2.py
+++ src/pytorch/test2.py
@@ -1,41 +1,15 @@
 from lark import Lark, Transformer, v_args
 import astpretty
 
-# Define the grammar
-# grammar = """
-#     start: classdef -> concat
-#     classdef: "class" CNAME "(" ALL suite -> contract
-#     suite: stmt+ -> suite
-#     stmt: funcdef -> stmt
-#     funcdef: (constructor | forward) -> stmt
-
-    
-#     constructor: "def __init__(" parameters ")" ":" -> constructor
-#     forward: "def forward(" parameters ")" ":" -> predict
-#     parameters: CNAME "," CNAME -> parameters
-    
-#     assign: (cassign | fassign) -> stmt
-#     cassign: "self" "." CNAME "=" expr -> cassign
-#     fassign: CNAME "=" ALL -> fassign
-
-#     expr: ALL -> expr
-#     return: "return" expr -> ret_val
-#     super: "super(" + ALL -> super
-#     CNAME: /[a-zA-Z_][a-zA-Z_0-9]*/
-#     ALL: /[^\\n]+/
-#     %import common.WS
-#     %ignore WS
-# """
-
 grammar = """
     start: classdef -> concat
     classdef: "class" CNAME "(" ALL suite -> contract
-    suite: stmt+ -> suite
-    stmt: (funcdef | assign | return) -> stmt
+    suite: funcdef+ -> suite
+    stmt: (assign | return | super) -> stmt
     funcdef: (constructor | forward) -> stmt
 
-    constructor: "def __init__(" parameters ")" ":" -> constructor
-    forward: "def forward(" parameters ")" ":" -> predict
+    constructor: "def __init__(" parameters ")" ":" stmt+-> constructor
+    forward: "def forward(" parameters ")" ":" stmt+-> predict
     parameters: CNAME "," CNAME -> parameters
     assign: (cassign | fassign) -> stmt
     cassign: "self" "." CNAME "=" expr -> cassign
@@ -49,8 +23,6 @@ grammar = """
     %import common.WS
     %ignore WS
 """
-
-# Define the transformer
 
 # cassign: "self" "." CNAME "=" linear
 # return: "return torch.sign(self.fc(x))" -> sign
@@ -66,16 +38,16 @@ class SolidityTransformer(Transformer):
         return ''.join(args)
 
     def stmt(self, funcdef):
-        return str(funcdef)
+        return '\t' + str(funcdef)
 
     def contract(self, name, inherit, func2):
         return f'contract {name} {{ \n {func2} }}\n'
 
-    def constructor(self, params):
-        return f'constructor({params}) {{\n}}\n'
+    def constructor(self, params, *stmts):
+        return f'constructor({params}) {{\n ' + '\n'.join(stmts) + '\n\t}}\n'
 
-    def predict(self, params):
-        return f'predicting({params}) {{\n}}\n'
+    def predict(self, params, *stmts):
+        return f'predicting({params}) {{\n ' + '\n'.join(stmts) + '\n\t}}\n'
 
     def parameters(self, x, y):
         return f'{x}, {y}'
@@ -84,22 +56,20 @@ class SolidityTransformer(Transformer):
         return str(x)
 
     def cassign(self, x, y):
-        return f'CAssign'
+        return f'CAssign {x} = {y}'
 
     def fassign(self, x, y):
-        return f'FAssign'
+        return f'FAssign {x} = {y}'
 
     def expr(self, x):
-        return f'Expression'
+        return f'{x}'
     
     def ret_val(self, x):
-        return f'return'
+        return f'\treturn {x}'
     
     def super(self, x):
-        return f'super'
+        return f'\tsuper({x}'
 
-
-# Create the parser
 
 
 
@@ -114,21 +84,16 @@ def py_to_solidity(expr):
     return transformer.transform(tree).strip()
 
 
-# "super(Perceptron, self).__init__()"
-
-# torch_code = """
-# class Perceptron(nn.Module):
-#     def forward(self, x):
-#     def __init__(self, input_dim):
-
-#     """
 
 torch_code = """
 class Perceptron(nn.Module):
     def __init__(self, input_dim):
+        super(Perceptron, self).__init__()
         self.fc = nn.Linear(input_dim, 1)
+        x = 5
 
     def forward(self, x):
+        y = 1
         return torch.sign(self.fc(x))"""
 # Testing the converter
 tree = get_ast(torch_code)
