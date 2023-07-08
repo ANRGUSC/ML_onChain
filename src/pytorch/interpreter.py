546399941692e3d469e074a517b6c7632ef2bdf6
Steve Vott <svott@usc.edu>
2023-07-07 20:34:53 -0700

    Interpreter for perceptron.sol

--- src/pytorch/interpreter.py
+++ src/pytorch/interpreter.py
@@ -1,12 +1,14 @@
 from lark import Lark, Transformer, v_args
 import astpretty
 from textwrap import dedent
+import subprocess
+import re
 
 
 grammar = """
     start: classdef -> concat
     classdef: "class" CNAME "(" ALL suite -> contract
-    suite: funcdef+ -> concat
+    suite: funcdef+ -> concat_line
     stmt: (assign | return | super) -> stmt
     funcdef: (constructor | forward) -> stmt
 
@@ -45,8 +47,17 @@ grammar = """
 
 @v_args(inline=True)
 class SolidityTransformer(Transformer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.contract_vars = []
+        self.setter_functions = []
+        self.variable_dims = {}
+
     def concat(self, *args):
         return ''.join(args)
+
+    def concat_line(self, *args):
+        return '\n'.join(args)
 
     def stmt(self, stmt):
         if stmt is None:
@@ -55,41 +66,80 @@ class SolidityTransformer(Transformer):
             return '\t' + str(stmt)
 
     def contract(self, name, inherit, func2):
-        return f'contract {name} {{ \n {func2} }}\n'
+        contract_var_str = '\n\t'.join(self.contract_vars)
+        setter_func_str = '\n\n\t'.join(self.setter_functions)
+        return f'contract {name} {{\n\t{contract_var_str}\n\n\t{setter_func_str}\n\n{func2} }}\n'
 
     def constructor(self, params : tuple, *stmts):
         typed_params = []
         for i in range(1, len(params)):
-            typed_params.append(f'int {params[i]}')
+            typed_params.append(f'uint {params[i]}')
 
         params_str = ', '.join(str(p) for p in typed_params)
-        return f'constructor({params_str}) {{\n ' + '\n'.join(filter(None, stmts)) + '\n\t}}\n'
+        return f'constructor({params_str}) {{\n ' + '\n'.join(filter(None, stmts)) + '\n\t}\n'
 
     def predict(self, params, *stmts):
         typed_params = []
         for i in range(1, len(params)):
-            typed_params.append(f'int {params[i]}')
+            typed_params.append(f'int[] memory {params[i]}')
 
         params_str = ', '.join(str(p) for p in typed_params)
-        return f'function predict({params_str}) returns (int[]) {{\n ' + '\n'.join(filter(None,stmts)) + '\n\t}}\n'
+        return f'function predict({params_str}) public view returns (int[] memory) {{' + '\n'.join(filter(None, stmts)) + '\n\t}\n' # TODO return stmt
 
     def parameters(self, x, y):
         return (x, y)
 
     def cassign(self, x, y):
-        return f'CAssign {x} = {y}'
+        var_type = 'int[]' if '[' in y and ']' in y else 'int'
+        if (y.count('[') == 2):
+            var_type = 'int[][]'
+        self.contract_vars.append(f'{var_type} public {x};')
+        if(var_type == 'int'):
+            assignment = f'{x} = {y};'
+            res = f'''
+            function set{x}({var_type} memory value) public {{
+                {x} = value;
+            }}'''
+        elif (var_type == 'int[]'):
+            sz = re.search(r'\[(.*?)\]', y).group(1)
+            self.variable_dims[x] = (sz)
+            assignment = f'{x} = new int[]({sz});'
+            res =  f'''
+    function set{x}({var_type} memory value) public {{
+        for (uint i = 0; i < value.length; ++i) {{
+            {x}[i] = value[i];
+        }}
+    }}'''
+        else: # int[][]
+            matches = re.findall(r'\[(.*?)\]', y)
+            if matches:
+                sz1, sz2 = matches
+            self.variable_dims[x] = (sz1, sz2)
+            assignment = f'{x} = new int[][]({sz1});\n' + \
+                f'for (uint i = 0; i < {sz1}; i++) {{\n' + \
+                f'\t{x}[i] = new int[]({sz2});\n' + \
+                f'}}\n'
+            res = f'''function set{x}({var_type} memory value) public {{
+                for (uint i = 0; i < value.length; ++i) {{
+                    for (uint j = 0; j < value[0].length; ++j) {{
+                        {x}[i][j] = value[i][j];
+                    }}
+                }}
+            }}'''
+        self.setter_functions.append(res)
+        return assignment
     
     # Pass assignment variables into contract
     # Generate update functions for each weight array
 
     def fassign(self, x, y):
-        return f'FAssign {x} = {y}'
+        return f'FAssign {x} = {y};'
 
     def expr(self, x):
         return f'{x}'
     
     def ret_val(self, x):
-        return f'\treturn {x}'
+        return f'return {x};'
     
     def super(self, x):
         return None
@@ -97,26 +147,55 @@ class SolidityTransformer(Transformer):
     def conv2d(self, *args):
         pass
     
+    # TODO, update Linear to support 2d arrays
     def linear(self, params):
-        res = f'int[{params[0]}][{params[1]}]'
+        if (params[1] == '1'):
+            res = f'int[{params[0]}]'
+        else:
+            res = f'int[{params[0]}][{params[1]}]'
         return res
 
     def layer_pass(self, layer, x):
-        res = f"""for (int i = 0; i < {layer}.length; ++i) {{
+        if layer in self.variable_dims:
+            dims = self.variable_dims[layer]
+            if (len(dims) == 2):
+                res = f"""
+        int[][] memory res = new int[][]({dims[1]});
+        for (uint i = 0; i < {dims[1]}; ++i) {{
+            int c = 0;
+            for (uint j = 0; j < {dims[0]}; ++j) {{
+                c += {layer}[i][j] * {x}[j];
+            }}
+            res[i] = c;
+        }}"""
+                return res
+            else: # length is 1
+                res = f"""
+        int[] memory res = new int[]({1});
+        int c = 0;
+        for (uint i = 0; i < {dims[0]}; ++i) {{
+            c += {layer}[i] * x[i];
+        }}
+        res[0] = c;"""
+                return res
+        else:
+            res = f"""
+            for (uint i = 0; i < {layer}.length; ++i) {{
                 int c = 0;
-                for (int j = 0; j < {layer}[0].length; ++j) {{
-                    c += {layer}[i][j] * x[j]
+                for (uint j = 0; j < {layer}[i].length; ++j) {{
+                    c += {layer}[i][j] * {x}[j];
                 }}
                 {x}[i] = c;
             }}"""
-        return res
+            return res
 
     def sign(self, x):
         res = f"""
-            for (int i = 0; i < x.length; ++i) {{
-                x[i] = ((x[i] >= 0) ? ((x[i] == 0) ? 0 : 1) : -1)
-            }}
-            """
+        for (uint i = 0; i < res.length; ++i) {{
+            res[i] = ((res[i] >= 0) ? ((res[i] == 0) ? int(0) : int(1)) : -1);
+        }}
+        return res;
+        """
         return x + res
 
     def dropout(self, x):
@@ -147,11 +226,13 @@ class Perceptron(nn.Module):
         return torch.sign(self.fc(x))"""
 # Testing the converter
 tree = get_ast(torch_code)
-print(tree.pretty())
-print(tree)
 
-output = py_to_solidity(torch_code)
+path = "../classifiers/perceptron.sol"
+output = "// SPDX-License-Identifier: UNLICENSED\npragma solidity >=0.4.22 <0.9.0;\n\n" + py_to_solidity(torch_code)
 print(output)
+f = open(path, "w")
+f.write(output)
+f.close()
 
 # TODO
 # Traverse ast before transformation, pass types to upper nodes in contract
