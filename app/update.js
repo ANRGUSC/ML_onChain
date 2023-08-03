a69c82a05e54cb6f2b7d6eafb684a9e3f6ae4f18
LazyTitan62 <leol@usc.edu>
2023-08-03 01:39:44 -0700

    added update function

--- app/update.js
+++ app/update.js
@@ -1,21 +1,36 @@
-const Web3 = require('web3');
+const {Web3} = require('web3');
+const contractData = require('../build/contracts/MultiPerceptron.json');  // Update the path if necessary
 
-// Instantiate web3 with Ganache
-const web3 = new Web3('http://localhost:7545');
+// Instantiate web3 with Ganache as provider
+const web3 = new Web3(new Web3.providers.HttpProvider('HTTP://127.0.0.1:7545'));
 
-//replace ABI with contract and contnractAddress with contract address
+// Get the ABI from the contractData
+const abi = contractData.abi;
+
+// Replace this with your contract's address
+const contractAddress = '0x98B487e45a127e220F801D2a93775B2ADeDf5294';
+
+// Create contract instance
 const contract = new web3.eth.Contract(abi, contractAddress);
 
-async function updateFcAndFc2() {
+async function updateFcFc2AndFc3() {
     const accounts = await web3.eth.getAccounts();
 
-    // Create new values for fc and fc2
+    // Create new values for fc, fc2 and fc3
     let newFc = [[1, 2], [3, 4]];
-    let newFc2 = [5];
+    let newFc2 = [[5, 6], [7, 8]];
+    let newFc3 = [9, 10];
 
-    // Call the setfc and setfc2 functions with the new values
-    await contract.methods.setfc(newFc).send({ from: accounts[0] });
-    await contract.methods.setfc2(newFc2).send({ from: accounts[0] });
+    // Call the setfc, setfc2 and setfc3 functions with the new values
+    //await contract.methods.setfc(newFc).send({ from: accounts[0] });
+    //await contract.methods.setfc2(newFc2).send({ from: accounts[0] });
+    //await contract.methods.setfc3(newFc3).send({ from: accounts[0] });
+
+    // removes gas limit
+    await contract.methods.setfc(newFc).send({ from: accounts[0], gas: 5000000 });
+    await contract.methods.setfc2(newFc2).send({ from: accounts[0], gas: 5000000 });
+    await contract.methods.setfc3(newFc3).send({ from: accounts[0], gas: 5000000 });
+
 }
 
-updateFcAndFc2();
+updateFcFc2AndFc3();
--- migrations/2_deploy_multipercetron.js
+++ migrations/2_deploy_multipercetron.js
@@ -2,5 +2,5 @@ var MultiPerceptron = artifacts.require("MultiPerceptron");
 
 module.exports = function(deployer) {
   // Pass 10 as the constructor argument for `input_dim`
-  deployer.deploy(MultiPerceptron, 10);
+  deployer.deploy(MultiPerceptron, 4);
 };
