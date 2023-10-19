3de160ef1a781dc4868c05e5b7f593dac3b1a2b5
LazyTitan62 <leol@usc.edu>
2023-10-19 01:07:40 -0700

    updated contract

--- contracts/classifiers/MLP_1.sol
+++ contracts/classifiers/MLP_1.sol
@@ -4,39 +4,51 @@ pragma solidity >=0.4.22 <0.9.0;
 import "../libraries/ABDKMath64x64.sol";
 
 contract MLP_1 {
-	int[][] public fc;
+    int[][] public fc_weights;
+    int[] public fc_biases;
 
     int[] public inputData;
     int[] public classifiedResults;
 
-    function inputDataAndLabels(int[] memory data) public {
-        inputData = data;
-    }
+    address public owner;
 
-    function setfc(int[][] memory value) public {
-        for (uint256 i = 0; i < value.length; ++i) {
-            for (uint256 j = 0; j < value[0].length; ++j) {
-                fc[i][j] = value[i][j];
-            }
-        }
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not the contract owner");
+        _;
     }
 
     constructor(uint256 input_dim) {
-        fc = new int[][](input_dim);
+        owner = msg.sender;
+        fc_weights = new int[][](input_dim);
         for (uint256 i = 0; i < input_dim; i++) {
-            fc[i] = new int[](2);
+            fc_weights[i] = new int[](2);
+        }
+        fc_biases = new int[](input_dim);
+    }
+
+    function inputDataAndLabels(int[] memory data) public onlyOwner {
+        inputData = data;
+    }
+
+    function setfc(int[][] memory weights, int[] memory biases) public onlyOwner {
+        require(weights.length == biases.length, "Weights and biases dimensions do not match");
+
+        for (uint256 i = 0; i < weights.length; ++i) {
+            for (uint256 j = 0; j < weights[i].length; ++j) {
+                fc_weights[i][j] = weights[i][j];
+            }
+            fc_biases[i] = biases[i];
         }
     }
 
     function predict(int x) public view returns (int) {
         int res = 0;
-        for (uint256 i = 0; i < 2; ++i) {
-            res += fc[i][0] * x;  // Adjusted this line
+        for (uint256 i = 0; i < fc_weights.length; ++i) {
+            res += fc_weights[i][0] * x + fc_biases[i];
         }
         res = res > 0 ? res : int256(0);  // Activation (ReLu)
         return res;
     }
-
 
     function classifyAndStore() public {
         require(inputData.length > 0, "No input data provided");
@@ -44,19 +56,11 @@ contract MLP_1 {
         classifiedResults = new int[](inputData.length);
 
         for (uint i = 0; i < inputData.length; i++) {
-            classifiedResults[i] = predict(inputData[i]); // Fixed this line
+            classifiedResults[i] = predict(inputData[i]);
         }
     }
-
 
     function getClassifiedResults() public view returns (int[] memory) {
         return classifiedResults;
     }
 }
-
-
-
-
-
-
-
