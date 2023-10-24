89295960a27715e2b6f3ced201174f76f076b7c4
LazyTitan62 <leol@usc.edu>
2023-10-24 00:37:46 -0700

    got PRB-Math to work

--- app/test.js
+++ app/test.js
@@ -2,7 +2,7 @@ const {Web3} = require('web3');
 const contractData = require('../build/contracts/MLP_Test.json');  // Replace with the path to your ABI
 
 const web3 = new Web3('HTTP://127.0.0.1:7545');  // Replace with your provider
-const contract = new web3.eth.Contract(contractData.abi, '0x9f0dDa3B3eB2072a54a445C49Fc0ee32d280a9A0');  // Replace with your contract's address
+const contract = new web3.eth.Contract(contractData.abi, '0xaCCcB71580008a65c1951AA545F703Eb38a43081');  // Replace with your contract's address
 
 function toPRBMathFormat(value) {
     return BigInt(Math.round(value * 10**18));
@@ -10,14 +10,14 @@ function toPRBMathFormat(value) {
 function fromPRBMathFormat(value) {
     return Number(value) / 10**18;
 }
-async function multiplyNumbers(value1, value2) {
+async function multiply(value1, value2) {
     const scaledValue1 = toPRBMathFormat(value1);
     const scaledValue2 = toPRBMathFormat(value2);
     console.log(scaledValue1);
     console.log(scaledValue2);
-    const result = await contract.methods.signedMul(5e18, 5.3e18).call();
+    const result = await contract.methods.multiply(scaledValue1, scaledValue2).call();
     console.log(fromPRBMathFormat(result));
 }
 
-multiplyNumbers(2.5, 1.1);
+multiply(2.5, 1.1);
 
--- contracts/classifiers/MLP_Test.sol
+++ contracts/classifiers/MLP_Test.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.4;
 
-import { SD59x18 } from "@prb/math/SD59x18.sol";
+import { SD59x18 } from "../../lib/prb-math/src/SD59x18.sol";
 
 contract MLP_Test {
 
