0afe4a467681cd05273c0da6b708a42e0e82e574
LazyTitan62 <leol@usc.edu>
2023-10-24 00:29:41 -0700

    added prb math

--- contracts/classifiers/MLP_Test.sol
+++ contracts/classifiers/MLP_Test.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.4;
 
-import "prb-math/contracts/PRBMath.sol";
+import { SD59x18 } from "@prb/math/SD59x18.sol";
 
 contract MLP_Test {
 
