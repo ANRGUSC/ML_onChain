4d7aeefe5d0223857dfeb3d1edebffc6e163671a
LazyTitan62 <leol@usc.edu>
2023-11-09 00:26:47 -0800

    updated MLP_2L_1N

--- contracts/MLP_3L_1N.sol
+++ contracts/MLP_3L_1N.sol
@@ -1,29 +1,37 @@
-//SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
-import { SD59x18 , convert, sd} from "../lib/prb-math/src/SD59x18.sol";
+import { SD59x18, convert, sd } from "../lib/prb-math/src/SD59x18.sol";
 
+contract MLP_3L_1N {
 
-contract MLP_2L_3N {
-
-    int256[][] public weights_layer1;  // 2D array for weights
+    // 2D array to store weights for each layer
+    int256[][] public weights_layer1;
     int256[][] public weights_layer2;
-    int256[][] public biases;     // 2D array for biases
+    int256[][] public weights_layer3;
+    // 2D array to store biases for each layer
+    int256[][] public biases;
+    // 2D array for training data
     int256[][] public training_data;
+    // Public variable to keep track of correct classifications
     int public correct_Count;
 
+    // Constructor to initialize the biases for 3 layers with 1 neuron each
     constructor(uint256 neurons) {
+        weights_layer1 = new int256[][](1);
+        weights_layer2 = new int256[][](1);
+        weights_layer3 = new int256[][](1);
         biases = new int256[][](neurons);
-        biases[0] = new int256[](3); // 1 neuron in the first layer
-        biases[1] = new int256[](1); // 1 neuron in the second layer
     }
 
+    // Function to set biases for a particular layer
     function set_Biases(uint256 layer, int256[] calldata b) external {
-        require(b.length == biases[layer].length, "Size of input biases does not match neuron number");
+        require(b.length == biases[layer].length, "Size of input biases does not match neuron number in the layer");
         biases[layer] = b;
     }
 
+    // Function to set weights for a particular layer
     function set_Weights(uint256 layer, int256[] calldata w) external {
-        require(layer < 2, "Layer index out of bounds");
+        require(layer < 3, "Layer index out of bounds");
         int256[] memory temp_w = new int256[](w.length);
         for (uint256 i = 0; i < w.length; i++) {
             temp_w[i] = w[i];
@@ -32,13 +40,12 @@ contract MLP_2L_3N {
             weights_layer1.push(temp_w);
         } else if (layer == 1) {
             weights_layer2.push(temp_w);
+        } else if (layer == 2) {
+            weights_layer3.push(temp_w);
         }
     }
 
-    function view_dataset_size() external view returns(uint256 size){
-        size = training_data.length;
-    }
-
+    // Function to add training data
     function set_TrainingData(int256[] calldata d) external {
         int256[] memory temp_d= new int256[](d.length);
         for (uint256 i = 0; i < d.length; i++) {
@@ -47,14 +54,19 @@ contract MLP_2L_3N {
         training_data.push(temp_d);
     }
 
-    // calculate the sigmoid of x
+    // Function to view the size of the dataset
+    function view_dataset_size() external view returns(uint256 size){
+        size = training_data.length;
+    }
+
+    // Sigmoid activation function
     function sigmoid(SD59x18 x) public pure returns (SD59x18) {
         int256 one = 1;
         SD59x18 one_cvt = convert(one);
         return (one_cvt).div(one_cvt.add((-x).exp()));
     }
 
-    //relu activation function
+    // ReLU activation function
     function relu(SD59x18 x) public pure returns (SD59x18) {
         int256 zero = 0;
         SD59x18 zero_cvt = convert(zero);
@@ -64,17 +76,17 @@ contract MLP_2L_3N {
         return zero_cvt;
     }
 
+    // Classification function
     function classify() public view returns (int) {
         int correct = 0;
 
         for (uint256 j = 0; j < 50; j++) {
             int256[] memory data = training_data[j];
             int256 label = data[0];
-
+            //-------------------------------------------------------------------------
             // Neuron results for Layer 1
-            SD59x18[] memory neuronResultsLayer1 = new SD59x18[](3);
-
-            for (uint256 n = 0; n < 3; n++) {
+            SD59x18[] memory neuronResultsLayer1 = new SD59x18[](1);
+            for (uint256 n = 0; n < 1; n++) {
                 neuronResultsLayer1[n] = SD59x18.wrap(biases[0][n]);
                 for (uint256 i = 1; i < data.length; i++) {
                     neuronResultsLayer1[n] = neuronResultsLayer1[n].add(SD59x18.wrap(data[i]).mul(SD59x18.wrap(weights_layer1[n][i-1])));
@@ -83,15 +95,25 @@ contract MLP_2L_3N {
             }
             //-------------------------------------------------------------------------
             // Neuron results for Layer 2
-            SD59x18 neuronResultLayer2 = SD59x18.wrap(biases[1][0]);
-            for (uint256 n = 0; n < 3; n++) {
-                neuronResultLayer2 = neuronResultLayer2.add(neuronResultsLayer1[n].mul(SD59x18.wrap(weights_layer2[0][n])));
+            SD59x18[] memory neuronResultsLayer2 = new SD59x18[](1);
+            for (uint256 n = 0; n < 1; n++) {
+                neuronResultsLayer2[n] = SD59x18.wrap(biases[1][n]);
+                for (uint256 i = 1; i < data.length; i++) {
+                    neuronResultsLayer2[n] = neuronResultsLayer2[n].add(SD59x18.wrap(data[i]).mul(SD59x18.wrap(weights_layer2[n][i-1])));
+                }
+                neuronResultsLayer2[n] = relu(neuronResultsLayer2[n]);
             }
-            neuronResultLayer2 = sigmoid(neuronResultLayer2);
+            //-------------------------------------------------------------------------
+            // Neuron results for Layer 3
+            SD59x18 neuronResultLayer3 = SD59x18.wrap(biases[2][0]);
+            for (uint256 n = 0; n < 1; n++) {
+                neuronResultLayer3 = neuronResultLayer3.add(neuronResultsLayer2[n].mul(SD59x18.wrap(weights_layer3[0][n])));
+            }
+            neuronResultLayer3 = sigmoid(neuronResultLayer3);
             //-------------------------------------------------------------------------
             int256 classification;
             SD59x18 point_five = sd(0.5e18);
-            if (neuronResultLayer2.gte(point_five)) {
+            if (neuronResultLayer3.gte(point_five)) {
                 classification = int256(1e18);
             } else {
                 classification = int256(0e18);
